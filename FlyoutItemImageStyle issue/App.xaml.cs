8afe6281ca3025e09f9a0584cc5d9a8812913969
Aliaksei Kartashou <aliakseikartashou@gmail.com>
2024-02-07 21:46:03 +0100

    Add project files.

---
+++ FlyoutItemImageStyle issue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlyoutItemImageStyle_issue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FlyoutItemImageStyle issue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlyoutItemImageStyle_issue
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
